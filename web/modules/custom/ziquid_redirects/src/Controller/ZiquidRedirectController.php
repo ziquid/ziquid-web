59522cdf536248c24765f1d5a23630e0663ef381
Joseph Cheek <joseph@ziquid.com>
2019-12-14 02:24:14 +0800

    phpcs errors from circleci.

--- web/modules/custom/ziquid_redirects/src/Controller/ZiquidRedirectController.php
+++ web/modules/custom/ziquid_redirects/src/Controller/ZiquidRedirectController.php
@@ -1,27 +1,29 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\ziquid_redirects\Controller\ZiquidRedirectController.
- */
 namespace Drupal\ziquid_redirects\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
-/*
+/**
  * Controller for custom ziquid URL redirects.
  */
 class ZiquidRedirectController extends ControllerBase {
+
+  /**
+   * Redirect for Zoom.
+   */
   public function zoomRedirect() {
     $response = new RedirectResponse('https://zoom.us/j/2110111973', 301);
     $response->send();
-    return;
   }
 
+  /**
+   * Redirect for iOS beta.
+   */
   public function iosBetaRedirect() {
     $response = new RedirectResponse('https://testflight.apple.com/join/vi0q9Ffy', 301);
     $response->send();
-    return;
   }
+
 }
