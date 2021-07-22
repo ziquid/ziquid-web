dc55ca6e0ee728a5d8eae24d5122a24a71d1eff6
Joseph Cheek <joseph@ziquid.com>
2021-07-21 23:05:31 -0600

    fixed scripts/build.sh for Mac OS.  Hopefully didn't break it for Linux this time.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,32 +2,38 @@
 # build ziquid on OVH3 or local
 
 set -xe
+cd $(dirname "$0")
 DIRNAME=$(pwd -P)
 BASENAME=$(basename "$0")
 FULLNAME="$DIRNAME/$BASENAME"
+UNAME=$(uname)
+
+function _linux() {
+  [ $UNAME == Linux ] && return 0
+  return 1
+}
 
 # Linux/non-apache?  sudo to apache
-# [ $(uname) == Linux ] && [ $(whoami) != apache ] && exec sudo su -l apache -s /bin/bash "$FULLNAME" "$@"
-[ $(uname) == Linux ] && [ $(whoami) != root ] && exec sudo su -l root -s /bin/bash "$FULLNAME" "$@"
+_linux && [ $(whoami) != apache ] && exec sudo su -l apache -s /bin/bash "$FULLNAME" "$@"
+# _linux && [ $(whoami) != root ] && exec sudo su -l root -s /bin/bash "$FULLNAME" "$@"
 
 function update() {
   drush cr -l $1 || :
   drush updb -y -l $1
   drush cr -l $1 || :
-  [ $(uname) == Linux ] && drush cim -y sync -l $1 || drush cim -y sync --partial -l $1
+  _linux && drush cim -y sync -l $1 || drush cim -y sync --partial -l $1
   drush cr -l $1
   drush cc views -l $1
   chown -R apache:apache .
 }
 
 # main()
-# cd $(dirname "$0")/..
-cd /var/www/html/ziquid/prod
+cd ..
 git fetch
 
 if [ "$1" != "" ]; then
   echo checking out branch $1...
-  git co $1
+  git checkout $1
   git pull
 fi
 
