02a53edea8d44d6e0d0d55545554ad5017a2c605
Joseph Cheek <joseph@ziquid.com>
2022-10-14 12:53:13 -0600

    bugfix for deployment script on linux

--- scripts/build.sh
+++ scripts/build.sh
@@ -30,7 +30,7 @@ function update() {
   drush cim -y sync -l $1 || drush cim -y sync -l $1
   drush cr -l $1
   drush cc views -l $1
-  _linux && chown -R www_data:www_data .
+  _linux && chown -R www-data:www-data .
 }
 
 # main()
